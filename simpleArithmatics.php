c5d7b27b8d537263c68cff2a370751cd54669dcb
Arindam Das Modak <arindam.modak03@gmail.com>
2017-03-08 22:57:20 +0530

    Add files via upload

---
+++ simpleArithmatics.php
@@ -0,0 +1,13 @@
+<?php 
+	$a=50;
+	$b=5;
+	$add=$a+$b;
+	$subtract=$a-$b;
+	$multiply=$a*$b;
+	$divide=$a/$b;
+	
+	echo "addition : $add <br>";
+	echo "subtration : $subtract <br>";
+	echo "multiplication : $multiply <br>";
+	echo "division : $divide <br>";
+?>
